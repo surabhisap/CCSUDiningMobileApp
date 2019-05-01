d7e30070dc94bfcfea34a7a7dc2cf8cae77819ff
Surabhi Agnihotri <surabhisap11@gmail.com>
2019-05-01 00:38:14 -0400

    Integrated Google and Apple maps to the diner halls. Added reviews count and updated write reviews option.

--- CCSUDining/CCSUDining/DiningHalls/DiningHallViewController.swift
+++ CCSUDining/CCSUDining/DiningHalls/DiningHallViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import CodableFirebase
+import MapKit
 
 class DiningHallViewController: UIViewController {
     
@@ -16,20 +17,40 @@ class DiningHallViewController: UIViewController {
     let viewModel = DinerViewModel()
     var diningHallArray = [String]()
     var menuArray = [String: [MenuModel]]()
+    var dinerReviewsArray = [String: ReviewsModel]()
     var selectedDinerIndex = 0
     private var dinerRatingDictionary = [String: String]()
+    private var currentLocation: CLLocation?
+    private let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getAndPopulateDinerData()
+        configureLocationManager()
+    }
+    
+    private func getAndPopulateDinerData() {
         
         viewModel.getMenuForToday(completionHandler: { [weak self] (menuModelArray) in
             self?.menuArray = menuModelArray
             self?.diningHallArray = [String] (menuModelArray.keys)
             
-            APIManager.shared.fetchDinerRatings(completionHandler: { [weak self] (dinerRatingDictionary) in
-                self?.dinerRatingDictionary = dinerRatingDictionary
+            APIManager().fetchDinerReviews { [weak self] (dinerReviews) in
+                self?.dinerReviewsArray = dinerReviews
+                for diner in dinerReviews {
+                    guard let allReviews = diner.value.reviews else {
+                        return
+                    }
+                    
+                    var totalReviews = 0
+                    for review in allReviews {
+                        totalReviews += Int(review.rating ?? "0") ?? 0
+                    }
+                    self?.dinerRatingDictionary[diner.key] = allReviews.count == 0 ? "0" : "\(totalReviews / allReviews.count)"
+                }
                 self?.diningTableView.reloadData()
-            })
+            }
         })
     }
     
@@ -42,17 +63,37 @@ class DiningHallViewController: UIViewController {
                 }
             }
         } else if segue.identifier == "dinerReviews" {
-            if let dinerReviewsViewController = segue.destination as? DinerReviewsViewController {
-                dinerReviewsViewController.dinerName = diningHallArray[selectedDinerIndex]
-                dinerReviewsViewController.dinerRating = dinerRatingDictionary[diningHallArray[selectedDinerIndex]] ?? "0"
+            if let dinerReviewsViewController = segue.destination as? DinerReviewsViewController, let tag = (sender as? UIButton)?.tag {
+                dinerReviewsViewController.dinerName = diningHallArray[tag]
+                dinerReviewsViewController.dinerRating = dinerRatingDictionary[diningHallArray[tag]] ?? "0"
+                dinerReviewsViewController.closure = { [weak self] in
+                    self?.getAndPopulateDinerData()
+                }
             }
         }
     }
     
-    @objc private func addReviewAction () {
-        performSegue(withIdentifier: "dinerReviews", sender: self)
+    @objc private func addReviewAction (sender: UIButton) {
+        performSegue(withIdentifier: "dinerReviews", sender: sender)
     }
+    
+    @objc private func locateDinerAction() {
 
+        let mapOptions = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        mapOptions.addAction(UIAlertAction(title: "Open in Apple Maps", style: .default , handler:{ [weak self] (UIAlertAction)in
+            self?.openDirectionsInAppleMaps()
+        }))
+        
+        mapOptions.addAction(UIAlertAction(title: "Open in Google Maps", style: .default , handler:{ [weak self] (UIAlertAction)in
+            self?.openDirectionsInGoogleMaps()
+        }))
+        
+        mapOptions.addAction(UIAlertAction(title: "Dismiss", style: .cancel , handler:{ (UIAlertAction)in
+        }))
+        
+        self.present(mapOptions, animated: true, completion: nil)
+    }
+    
 }
 
 
@@ -71,19 +112,13 @@ extension DiningHallViewController: UITableViewDelegate, UITableViewDataSource {
         
         cell.dinerNameLabel.text = DiningHallType(rawValue: diningHallArray[indexPath.row])?.hallName
         cell.addReviewButton.addTarget(self, action: #selector(addReviewAction), for: .touchUpInside)
+        cell.locateDinerButton.addTarget(self, action: #selector(locateDinerAction), for: .touchUpInside)
         cell.dinerRatingView.rating = Double(dinerRatingDictionary[diningHallArray[indexPath.row]]!)!
+        cell.addReviewButton.setTitle("Add Reviews(\(dinerReviewsArray[diningHallArray[indexPath.row]]?.reviews?.count ?? 0))", for: .normal)
+        cell.dinerImageView.image = UIImage(named: diningHallArray[indexPath.row])
+        cell.addReviewButton.tag = indexPath.row
         
         return cell
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        
-        let headerCell = tableView.dequeueReusableCell(withIdentifier: "headerCell")
-        return headerCell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -109,4 +144,54 @@ enum DiningHallType: String, CaseIterable {
         }
     }
     
+    func getDinerLatLong() -> CLLocationCoordinate2D {
+        switch self {
+        case .hilltop_cafe:
+            return CLLocationCoordinate2D(latitude: 41.6938183, longitude: -72.7637449)
+        case .memorial_hall:
+            return CLLocationCoordinate2D(latitude: 41.6933, longitude: -72.7642)
+        case .devils_den:
+            return CLLocationCoordinate2D(latitude: 41.6933, longitude: -72.7642)
+        }
+    }
+    
+}
+
+extension DiningHallViewController: CLLocationManagerDelegate {
+    
+    private func configureLocationManager() {
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    private func openDirectionsInGoogleMaps() {
+        
+        guard let selectedDinerCoordinate = DiningHallType(rawValue: diningHallArray[selectedDinerIndex])?.getDinerLatLong() else { return }
+        if let url = URL(string: "comgooglemaps://?saddr=&daddr=\(selectedDinerCoordinate.latitude),\(selectedDinerCoordinate.longitude)&directionsmode=walking") {
+            UIApplication.shared.open(url, options: [:])
+        }
+    }
+    
+    private func openDirectionsInAppleMaps() {
+        
+        guard let currentLocation = locationManager.location?.coordinate else { return }
+        guard let selectedDinerCoordinate = DiningHallType(rawValue: diningHallArray[selectedDinerIndex])?.getDinerLatLong() else { return }
+        let source = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: currentLocation.latitude, longitude: currentLocation.longitude)))
+        source.name = "Current Location"
+        
+        let destination = MKMapItem(placemark: MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: selectedDinerCoordinate.latitude, longitude: selectedDinerCoordinate.longitude)))
+        destination.name = DiningHallType(rawValue: diningHallArray[selectedDinerIndex])?.hallName
+        
+        MKMapItem.openMaps(with: [source, destination], launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeWalking])
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        manager.stopUpdatingLocation()
+    }
 }
