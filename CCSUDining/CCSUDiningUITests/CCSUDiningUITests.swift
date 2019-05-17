9a1eb228b687ea3bdb945923ab66d6c9135b1468
Surabhi Agnihotri <surabhisap11@gmail.com>
2019-05-17 00:09:38 -0400

    About US and Few more UI test

--- CCSUDining/CCSUDiningUITests/CCSUDiningUITests.swift
+++ CCSUDining/CCSUDiningUITests/CCSUDiningUITests.swift
@@ -25,6 +25,70 @@ class CCSUDiningUITests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testSignUp() {
+        
+        let app = XCUIApplication()
+        app.tabBars.buttons["My Account"].tap()
+        
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element.swipeUp()
+        
+        app.buttons["SIGN UP USING YOUR EMAIL"].tap()
+        
+        let firstNameTextField = app.textFields["First Name"]
+        firstNameTextField.tap()
+        firstNameTextField.typeText("surabhi")
+        
+        let lastNameTextField = app.textFields["Last Name"]
+        lastNameTextField.tap()
+        lastNameTextField.typeText("agnihotri")
+        
+        
+        let emailTextField = app.textFields["Email"]
+        emailTextField.tap()
+        emailTextField.typeText("sura@my.ccsu.edu")
+        
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("surabhi@ccsu")
+        
+        let rePasswordSecureTextField = app.secureTextFields["Re-enter Password"]
+        rePasswordSecureTextField.tap()
+        rePasswordSecureTextField.typeText("surabhi@ccsu")
+        
+        element.swipeUp()
+        element.tap()
+        app.buttons["SIGN UP"].tap()
+        
+        sleep(4)
+        app.tables.buttons["Logout"].tap()
+        
+    }
+    
+    func testSignIn() {
+        // Use recording to get started writing UI tests.
+        
+        let app = XCUIApplication()
+        app.tabBars.buttons["My Account"].tap()
+        
+        let signInButton = app.buttons["Sign In"]
+        signInButton.tap()
+        
+        let emailTextField = app.textFields["Email"]
+        emailTextField.tap()
+        emailTextField.typeText("surabhi.sgo@gmail.com")
+        
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText("surabhi@sgo")
+        
+        signInButton.tap()
+        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .table).element.swipeUp()
+        app.tables.buttons["Logout"].tap()
+        
+    }
+    
 
     func testMealTypeFilter() {
         // Use recording to get started writing UI tests.
@@ -74,88 +138,64 @@ class CCSUDiningUITests: XCTestCase {
         
         let menuButton = app.navigationBars["Nutritional Information"].buttons["Menu"]
         menuButton.tap()
-     
         
     }
-    
-    func testSignIn() {
-        // Use recording to get started writing UI tests.
-        
-        let app = XCUIApplication()
-        app.tabBars.buttons["My Account"].tap()
-        
-        let signInButton = app.buttons["Sign In"]
-        signInButton.tap()
-        
-        let emailTextField = app.textFields["Email"]
-        emailTextField.tap()
-        emailTextField.typeText("surabhi.sgo@gmail.com")
-        
-        let passwordSecureTextField = app.secureTextFields["Password"]
-        passwordSecureTextField.tap()
-        passwordSecureTextField.typeText("surabhi@sgo")
-        
-        signInButton.tap()
-        app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .table).element.swipeUp()
-        app.tables.buttons["Logout"].tap()
-        
-    }
-    
-    func testSignUp() {
-        // Use recording to get started writing UI tests.
-        
-        
-        let app = XCUIApplication()
-        app.tabBars.buttons["My Account"].tap()
-        
-        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
-        element.swipeUp()
-        
-        app.buttons["SIGN UP USING YOUR EMAIL"].tap()
-        
-        let firstNameTextField = app.textFields["First Name"]
-        firstNameTextField.tap()
-        firstNameTextField.typeText("surabhi")
-        
-        let lastNameTextField = app.textFields["Last Name"]
-        lastNameTextField.tap()
-        lastNameTextField.typeText("agnihotri")
-        
-        
-        let emailTextField = app.textFields["Email"]
-        emailTextField.tap()
-        emailTextField.typeText("surabhiagnihotri@my.ccsu.eu")
-        
-        let passwordSecureTextField = app.secureTextFields["Password"]
-        passwordSecureTextField.tap()
-        passwordSecureTextField.typeText("surabhi@ccsu")
-        
-        let rePasswordSecureTextField = app.secureTextFields["Re-enter Password"]
-        rePasswordSecureTextField.tap()
-        rePasswordSecureTextField.typeText("surabhi@ccsu")
-        
-        element.swipeUp()
-        element.tap()
-        app.buttons["SIGN UP"].tap()
-
-        sleep(4)
-        app.tables.buttons["Logout"].tap()
-       
-    }
-    
     
     func testMenuFavorite() {
         // Use recording to get started writing UI tests.
         
-    }
-    
-    func testAddReviews() {
-        // Use recording to get started writing UI tests.
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Hilltop Cafe"]/*[[".cells.staticTexts[\"Hilltop Cafe\"]",".staticTexts[\"Hilltop Cafe\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
+        let dicedSmokedHamRedPotatoesCabbageChickenBrothCeleryGarlicAndFreshThymeStaticText = tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Diced smoked ham, red potatoes, cabbage, chicken broth, celery, garlic and fresh thyme"]/*[[".cells.staticTexts[\"Diced smoked ham, red potatoes, cabbage, chicken broth, celery, garlic and fresh thyme\"]",".staticTexts[\"Diced smoked ham, red potatoes, cabbage, chicken broth, celery, garlic and fresh thyme\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        dicedSmokedHamRedPotatoesCabbageChickenBrothCeleryGarlicAndFreshThymeStaticText.tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Sodium"]/*[[".cells.staticTexts[\"Sodium\"]",".staticTexts[\"Sodium\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(2)
+        app.alerts["Smoked Ham, Cabbage & Potato Soup added to favroite"].buttons["Ok"].tap()
+        app.tabBars.buttons["My Account"].tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Favorites"]/*[[".cells.staticTexts[\"Favorites\"]",".staticTexts[\"Favorites\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        dicedSmokedHamRedPotatoesCabbageChickenBrothCeleryGarlicAndFreshThymeStaticText.tap()
+        app.alerts["Smoked Ham, Cabbage & Potato Soup is available today in Hilltop Cafe"].buttons["Ok"].tap()
+        sleep(2)
     }
     
     func testViewReviews() {
         // Use recording to get started writing UI tests.
         
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Add Reviews(10)"]/*[[".cells.buttons[\"Add Reviews(10)\"]",".buttons[\"Add Reviews(10)\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let addReviewsForHilltopCafeNavigationBar = app.navigationBars["Add Reviews for Hilltop Cafe"]
+        addReviewsForHilltopCafeNavigationBar.buttons["customerReviews"].tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"super cool")/*[[".cells.containing(.staticText, identifier:\"I like this app\")",".cells.containing(.staticText, identifier:\"super cool\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.otherElements["Rating"].swipeUp()
+        tablesQuery.children(matching: .cell).element(boundBy: 2).otherElements["Rating"].swipeDown()
+        app.navigationBars["CCSUDining.ViewDinerReviews"].buttons["Add Reviews for Hilltop Cafe"].tap()
+        addReviewsForHilltopCafeNavigationBar.buttons["Diners"].tap()
+        
     }
+    
+    func testAppFeedback() {
+        // Use recording to get started writing UI tests.
+        
+        let app = XCUIApplication()
+        app.tabBars.buttons["My Account"].tap()
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["App Feedback"]/*[[".cells.staticTexts[\"App Feedback\"]",".staticTexts[\"App Feedback\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let feedbackTitleTextField = app.textFields["Feedback Title"]
+        feedbackTitleTextField.tap()
+        feedbackTitleTextField.typeText("Thanks guys for this awesome app")
+        
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        let feedbackCommentTextView = element.children(matching: .textView).element
+        feedbackCommentTextView.tap()
+        feedbackCommentTextView.typeText("I really like the filter feature of this app, now I can easily sort, search the food I want.")
+        sleep(2)
+        element.tap()
+        app.buttons["Submit"].tap()
+        app.alerts["Thank you"].buttons["OK"].tap()
+        
+    }
+
 }
